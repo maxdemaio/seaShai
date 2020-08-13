ab2839a2238a0901fa7fcdd6423ff034158ac65c
Maxwell DeMaio <maxdman321@gmail.com>
2020-08-12 23:12:50 -0400

    Added spaces to sushi a la carte special rolls, started next section

--- menuScrub/sushiScrub.py
+++ menuScrub/sushiScrub.py
@@ -1,73 +1,43 @@
 # Parse all the menu data
 
-appetizers = """<h3>Fresh</h3>
-            Dr 1 Maguro - Tuna 6.00
-            Dr 2 Hamachi - Yellow Tail 6.00
-            Dr 3 Sake - Salmon 5.00
-            Dr 4 Hirame - Fluke 5.00
-            Dr 5 Toro - Tuna Belly M/P
-            Dr 6 Amaebi - Sweet Shrimp 6.00
-            Dr 7 Tai - Red Snapper 4.50
-            Dr 8 Hocky Gai - Surf Clam 5.50
-            Dr 9 Saba - Mackerel 4.50
-            Dr 10 Suzuki - Sea Bass 4.50
-            Dr 11 Ika - Squid 4.50
-            Dr 12 Hotate - Scallop 6.50
-            Dr 13 Shiro Maguro - White Tuna 5.50
-        <h3>Smoked/Cooked</h3>
-            Dr 14 Sake - Smoked Salmon 6.00
-            Dr 15 Tako - Octopus 6.00
-            Dr 16 Kani Kama - Crab Stick 4.00
-            Dr 17 Ebi - Shrimp 5.00
-            Dr 18 Unagi - Eel 5.00
-            Dr 19 Tamago - Egg 4.00
-        <h3>Roe</h3>
-            Dr 20 Ikura - Salmon Roe 7.00
-            Dr 21 Tobiko - Flying Fish Roe 5.50
-            Dr 22 Uni - Sea Urchin 8.00
-            Dr 23 Uzura - Quail Egg 2.00
-        <h3>Vegetable Maki (8 pcs/order)</h3>
-            Dr 24 Kappa - Cucumber 4.00
-            Dr 25 Oshinko - Pickled Radish 4.00
-            Dr 26 Yamagobo - Pickled Gobo Root4.00
-            Dr 27 Avocado - Avocado 4.00
-            Dr 28 Kampyo - Squash 4.00
-            Dr 29 Mushroom - Mushroom 4.00
-            Dr 30 Spinach - Spinach 4.00
-            Dr 31 Sakura - Avo,cuc,carrot 5.00
-        <h3>Inside Maki (seaweed on outside)</h3>
-            Dr 32 Tekka - Tuna Roll 5.00
-            Dr 33 Sake - Salmon 5.00
-            Dr 34 Tobiko - Maki Flying Fish Roe 6.00
-            Dr 35 Futomaki - Vegetable only 6.00 w/egg ,crab stick 7.00
-        <h3>Inside Out Maki (rice on outside)</h3>
-            Dr 36 California - crab,avo,fish roe cuc 5.00
-            Dr 37 Boston - salmon crab,mayo,cuc, 5.50
-            Dr 38 Manhattan - smoked salmon,avo 6.50
-            Dr 39 Mexican - shr temp, cuc, hotsauce 6.00
-            Dr 40 Negihamachi - yellowtail,scallions 8.00
-            Dr 41 Unagi w/avo - eel w/avo 6.00
-            Dr 42 Unagi w/cuc - eel w/cuc 6.00
-            Dr 43 Spicy Tekka - tuna w/cuc 7.00
-            Dr 44 Spicy Hotate - scallop, hot sauce 8.00
-            Dr 45 New Sea Shai - 5 different fish w/crab, avo 11.00
-            Dr 46 Sake Kawa - salmon skin, cuc 6.00
-            Dr 47 Shrimp w/cucumber 6.00
-            Dr 48 Soft Shell Crab Roll 8.00
-            Dr 49 Philadelphia - smoked salmon w/ cream cheese 6.50
-            Dr 50 Indiana - flying fish roe crabstick, avocado, w/mayo 6.00
-        <h3>Special Rolls</h3>
-            Dr51 Hamachi Jalapeno 8.00 
-            Dr52 Spicy Crunch 12.00
-            Dr53 Dragon Roll -- Shrimp tempura & eel , avocado on top 14.00
-            Dr54 Dynamite Roll -- grilled tako, crab, tobiko , scallions 8.00
-            Dr55 Crunchy Munchy -- Crab, cucumber, tempura flake w/ mayo sauce 8.00
-            Dr56 Philly Dream Roll -- shrimp tempura, cream cheese, tobiko w/ eel sauce 9.00
-            Dr57 360 Philadelphia -- cream cheese, asparagus topped w/ tuna and deep fried 11.00
-            Dr58 Rainbow -- assortment of fish w/ crab, avo, cuc 16.00
-            Dr59Spider Roll -- Jumbo soft shell crab, cuc, & flying fish roe 15.00
-            Dr60 Caterpillar Roll -- layers of avocado w/ eel & cucumber 10.00
-            Dr61 Crazy Dragon -- Shrimp Tempura, cucumber, and avo, spicy tuna on top w/ spicy mayo 15.00
+appetizers = """<h3>Appetizers</h3>
+            E1 Sushi Appetizer Assorted slices of raw fish on seasoned rice (4pcs) 9.00
+            E2 Sashimi Appetizer Assorted slices of raw fish (9 pcs) 13.00
+            E3 Seaweed Salad Seaweed and cucumbers in a vinegar sauce 5.00
+            E4 Sunomono Assorted slices of raw fish and seaweed in a vinegar sauce 10.00
+            E5 Kani-Su Crabstick rolled inside cucumbers 7.00
+            E6 Tako-Su Octopus in a vinegar sauce 9.00
+            E7 Ebi-Su Shrimp in a vinegar sauce 9.00
+            E8 Tuna-Su Tuna rolled inside cucumber 10.00
+            E9 Tuna Tataki Grilled Tuna w/ Ponzu Sauce 13.00
+            E10 Tuna Tartar Fresh tuna green onion and radish w/ Ponzu Sauce 13.00
+            E11 Salmon Tartar Fresh salmon, avocado, and onion w/ avocado sauce 9.00
+        <h3>Entrees</h3>
+            E12 Sushi Assorted slices of raw fish on seasoned rice 
+            Regular 21.00
+            Deluxe 25.00
+            E13 Sashimi Assorted slices of raw fish 
+            Regular 23.00
+            Deluxe 29.00
+            E14 Moriawase Combination of sushi and sashimi w/ California Roll 24.00
+            E15 Chirashi Sushi Seasoned rice topped w/ assorted raw fish 20.00
+            E16 Tekka-Don Seasoned rice topped w/ tuna 20.00
+            E17 Una-Don Seasoned rice topped w/ eel 20.00
+            E18 Makimono Combination 
+            California,Tekka, Kappa - Regular 14.00
+            new Sea Shai, California, Tekka, Kappa - Deluxe 23.00
+            E19 Hae-Dup Bob Assorted slices of raw fish and vegetables with a dynamite sauce 18.00
+        <h3>Special Combinations</h3>
+            New Sea Shai Boat Assortment of Sashimi, Sushi, &new Sea Shai Roll
+            F1 5pcs Sushi, 6pcs Sashimi, &new Sea Shai Roll Small 29.00
+            F2 8pcs Sushi, 9pcs Sashimi, new Sea Shai Roll & Calif. Roll Medium 39.00
+            F3 10pcs Sushi, 12pcs Sashimi, 3 rolls & new Sea Shai Roll Large 65.00
+            F4 12pcs Sushi, 18pcs Sashimi, 4 rolls & new Sea Shai Roll King 85.00
+            New Sea Shai Combination Assortment of Sushi and Maki
+            F5 5pcs Sushi, new Sea Shai Roll, & California Roll Small 29.00
+            F6 8pcs Sushi, newSea Shai Roll, Calif., Tekka, &Kappa Rolls Medium 39.00
+            F7 10pcs Sushi,new Sea Shai Roll, Calif., Tekka, Kappa, Futomaki, & Sake w/Cucumber Rolls Large 65.00
+            F8 12pcs Sushi, new Sea Shai Rolls & 8 rolls Queen 85.00 
     </div>
 """
 ## Preliminary lists
